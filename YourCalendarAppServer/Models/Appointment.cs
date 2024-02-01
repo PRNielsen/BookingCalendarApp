e69e27ac15206d8fb739a97f49d117d6b7e1bdf4
peter <peter@NIELSEN>
2024-02-02 00:58:03 +0100

    Add project files.

---
+++ YourCalendarAppServer/Models/Appointment.cs
@@ -0,0 +1,11 @@
+﻿namespace YourCalendarAppServer.Models
+{
+    public class Appointment
+    {
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Service SelectedService { get; set; }
+        public string Note { get; set; }
+    }
+}
---
+++ YourCalendarAppServer/Models/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace YourCalendarAppServer.Models
+{
+    public class Service
+    {
+        public TimeSpan Duration { get; set; }
+        public double Price { get; set; }
+        public string Name { get; set; }
+    }
+}
